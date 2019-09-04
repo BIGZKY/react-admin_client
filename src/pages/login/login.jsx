468334f5a55de0560835ad3a63db429f88a83d1a
zky <1253673851@qq.com>
2019-09-04 18:44:30 +0800

    form verify

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -10,11 +10,20 @@ import './login.less'
 /**
  * 登录组件
  *  */
-export default class Login extends Component {
-    handleSubmit = (e) => {
+class Login extends Component {
 
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const form = this.props.form;
+        console.log(form.getFieldsValue())
+    }
+    validatorPwd = (rule,value,callback) => {
+        if(!value){
+            callback('mu')
+        }
     }
     render() {
+        const { getFieldDecorator } = this.props.form;
         return (
             <div className="loginWrap">
                 <header className="login-header">
@@ -25,16 +34,35 @@ export default class Login extends Component {
                     <h2>用户登录</h2>
                     <Form onSubmit={this.handleSubmit} className="login-form">
                         <Form.Item>
-                            <Input
-                                prefix={<Icon type="user" style={{color:'rgba(0,0,0,.25)'}}></Icon>}
-                                placeholder="userName"
-                            />
+                            {getFieldDecorator('username', {
+                                rules: [
+                                        {required: true, message: '请输入用户名!'},
+                                        {max: 12, message: '用户名最多12位!'},
+                                        {min: 4, message: '用户名最少4位!'},
+                                        {pattern: /^[0-9a-zA-Z_]+$/, message: '用户名必须有数字, 字母组成!'},
+                                    ],
+                            })(
+                                <Input
+                                    prefix={<Icon type="user" style={{color:'rgba(0,0,0,.25)'}}></Icon>}
+                                    placeholder="userName"
+                                />
+                            )}
+                            
                         </Form.Item>
                         <Form.Item>
-                            <Input
-                                prefix={<Icon type="lock" style={{color:'rgba(0,0,0,.25)'}}></Icon>}
-                                placeholder="password" type="password"
-                            />
+                            {getFieldDecorator('password', {
+                                rules: [
+                                    {
+                                        validator: this.validatorPwd
+                                    }
+                                ]
+                            })(
+                                <Input
+                                    prefix={<Icon type="lock" style={{color:'rgba(0,0,0,.25)'}}></Icon>}
+                                    placeholder="password" type="password"
+                                />
+                            )}
+                            
                         </Form.Item>
                         <Form.Item>
                             <Button type="primary" htmlType="submit" className="login-form-button">登录</Button>
@@ -45,3 +73,12 @@ export default class Login extends Component {
         )
     }
 } 
+
+/**
+ * 高阶组件
+ *  1, 本质上是一个函数
+ *  2, 接收一个组件(被包装组件) , 返回一个新的组件(包装组件), 包装组件会向被包装组件传递特定的属性
+ *  3, 作用: 扩展组件的功能
+ */
+
+export default Login = Form.create()(Login);
