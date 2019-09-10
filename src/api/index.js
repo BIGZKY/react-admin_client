ddd0fbc676f4b0c42ce0760027066753870f0507
zky <1253673851@qq.com>
2019-09-10 17:59:47 +0800

    header layout

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,34 @@
-import ajax from "./ajax";
-
+import ajax from "./ajax"
+import jsonp from 'jsonp'
+import {message} from 'antd'
 const BASE = '';
 
 export const reqLogin = (username,password) => ajax(BASE+'/login',{username,password},'POST');
+
+//json 请求的接口请求函数
+/**
+ * jsonp 是解决get类型的ajax跨域请求问题
+ * jsonp 请求不是ajax请求, 而是一般的get请求
+ * 基本原理
+ *     浏览器端:
+ *         动态生成<script> 来请求后台接口(src就是接口的url)
+ *         定义好用于接收响应数据的函数(fn), 并将函数名通过请求参数提交给后台(如: callbak=fn)
+ *     服务器端:
+ *          接收到请求处理产生结果数据后, 返回一个函数调用的js代码, 并将结果作为实参传入函数调用
+ *     浏览器端:
+ *          收到响应后自动执行函数调用的js代码, 也就是执行了提前定义好的回调函数,并得到了需要的结果数据
+ */
+ export const reqWeather = (city) => {
+    const url = `http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`;
+    return new Promise((resolve, reject)=>{
+        jsonp(url, {}, (err, data)=>{
+
+            if(!err && data.status === 'success'){
+                const {dayPictureUrl, weather} = data.results[0].weather_data[0]
+                resolve({dayPictureUrl, weather})
+            } else {
+                message.error('获取天气信息失败!')
+            }
+        })
+     })
+ }
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,26 +1,103 @@
 import React, { Component } from "react"
-import {modal,Button } from 'antd'
+import {Modal,Button } from 'antd'
+import {withRouter, Redirect} from 'react-router-dom'
 
+import store from '../../utils/storageUtils'
+import {reqWeather} from '../../api/index'
+import {formateDate} from '../../utils/dateUtils'
+import memoryUtils from '../../utils/memmoryUtils'
+import menuList from '../../config/menuConfig'
 import './index.less'
 
-export default class Header extends Component {
+class Header extends Component {
+
+    state = {
+      currentTime: formateDate(new Date()),
+      dayPictureUrl: '',
+      weather: ''
+    }
+    componentWillMount() {
+      this.getWeather()
+    }
+    getWeather = async () => {
+      //调用接口请求异步获取
+      const {dayPictureUrl, weather} = await reqWeather('郑州');
+      this.setState({
+        dayPictureUrl,
+        weather
+      })
+    }
+    getTime = () => {
+      this.intervalId = setInterval(()=> {
+        const currentTime = formateDate(new Date());
+        this.setState({currentTime})
+      },1000)
+    }
+    getTitle= () => {
+      let path = this.props.location.pathname;
+      let title = '';
+      menuList.forEach((item) => {
+        if(item.key === path) {
+          title = item.title
+        }else if(item.children){
+          
+          let currentPath = item.children.find((items)=>{
+              return items.key === path;
+          })
+          if(currentPath) {
+            title = currentPath.title;
+          }
+        }
+      })
+      return title
+    }
+    //退出登录
+    logout = () => {
+      //显示确认框
+      Modal.confirm({
+        title: '确定退出吗?',
+        onOk: () => {
+          store.removeUser();
+          memoryUtils.user = {};
+          this.props.history.replace('/login');
+        }
+      })
+    }
+    /**
+     * 当前组件卸载之前调用
+     */
+    componentWillUnmount() {
+      clearInterval(this.intervalId)
+    }
+    /**
+     * 第一次render() 执行执行
+     * 一般在此执行异步操作: 发ajax请求/启动定时器
+     */
+    componentDidMount() {
+      this.getTime();
+    }
     render() {
-        return (
-            <div className="header">
-              <div className="header-top">
-                <span>欢迎, admin</span>
-                <Button onClick={this.logout}>退出</Button>
-              </div>
-              <div className="header-bottom">
-                <div className="header-bottom-left">首页</div>
-                <div className="header-bottom-right">
-                  {/* <span>{currentTime}</span>
-                  <img src={dayPictureUrl} alt="weather"/>
-                  <span>{weather}</span> */}
-                </div>
+      const username = memoryUtils.user.username;
+      const {currentTime, dayPictureUrl, weather} = this.state;
+      const title = this.getTitle();
+      return (
+          <div className="header">
+            <div className="header-top">
+              <span>欢迎, {username}</span>
+              <Button onClick={this.logout}>退出</Button>
+            </div>
+            <div className="header-bottom">
+              <div className="header-bottom-left">{title}</div>
+              <div className="header-bottom-right">
+                <span>{currentTime}</span>
+                <img src={dayPictureUrl} alt="weather"/>
+                <span>{weather}</span>
               </div>
             </div>
-          )
+          </div>
+        )
     }
     
 } 
+
+export default withRouter(Header)
