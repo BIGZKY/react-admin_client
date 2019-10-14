ea3efd2ac7e5e09dcdc0e5c8ee390680d4799b09
zky <1253673851@qq.com>
2019-10-15 01:01:01 +0800

    10-15

--- views/src/pages/product/add-update.jsx
+++ views/src/pages/product/add-update.jsx
@@ -1,21 +1,22 @@
 import React, { Component } from "react"
 import {
     Form,
-    Slider,
     Select,
     Button,
     Icon,
     Input,
-    InputNumber,
     Card,
     Upload,
-    Modal 
+    Modal,
+    Cascader
   } from 'antd';
 
 import "./add-update.less";
 import LinkButton from "../../components/link-button/link-button";
+import {reqCategorys} from '../../api'
 const { TextArea } = Input;
 const { Option } = Select;
+
 class AddUpdate extends Component {
     state = {
         previewVisible: false,
@@ -27,31 +28,63 @@ class AddUpdate extends Component {
             status: 'done',
             url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
           },
-          {
-            uid: '-2',
-            name: 'image.png',
-            status: 'done',
-            url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-          },
-          {
-            uid: '-3',
-            name: 'image.png',
-            status: 'done',
-            url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-          },
-          {
-            uid: '-4',
-            name: 'image.png',
-            status: 'done',
-            url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-          },
-          {
-            uid: '-5',
-            name: 'image.png',
-            status: 'done',
-            url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-          },
         ],
+        options: []
+    }
+    loadData = async selectedOptions => {
+      //得到选择的option 对象
+      const targetOption = selectedOptions[0];
+      //显示loading
+      targetOption.loading = true;
+
+      console.log(targetOption)
+      //请求异步获取
+      const result = await this.reqCategorys(targetOption.value);
+      targetOption.loading = false;
+      if(result && result.length>0){
+        let subOptions = result.map((c) => ({
+          value: c._id,
+          label: c.categoryName,
+          isLeaf: true
+        }))
+        targetOption.children = subOptions
+      }else{
+        targetOption.isLeaf = true;
+        
+      }
+      this.setState({
+        options: [...this.state.options]
+      })
+     
+    }
+    componentDidMount() {
+      this.reqCategorys('0')
+    }
+    reqCategorys = async (parentId) => {
+      const result = await reqCategorys(parentId); 
+      let options;
+      if(parentId==='0'){
+        options = result.data.map((c) => ({
+          value: c._id,
+          label: c.categoryName,
+          isLeaf: false
+        }))
+        this.setState({
+          options
+        })
+      }else{
+        return result.data;
+      }
+    }
+    /**
+     * 对价格自定义验证
+     */
+    validatorPrice = (rule, value, callback) => {
+      if(value*1 > 0){
+        callback() //验证通过
+      }else{
+        callback('价格必须大于0');
+      }
     }
     handleCancel = () => this.setState({ previewVisible: false })
 
@@ -65,7 +98,7 @@ class AddUpdate extends Component {
         previewVisible: true,
       });
     }
-  
+    
     handleChange = ({ fileList }) => this.setState({ fileList })
     handleSubmit = e => {
         e.preventDefault();
@@ -81,8 +114,8 @@ class AddUpdate extends Component {
       render() {
         const { getFieldDecorator } = this.props.form;
         const formItemLayout = {
-          labelCol: { span: 6 },
-          wrapperCol: { span: 14 },
+          labelCol: { span: 2 },
+          wrapperCol: { span: 8 },
         };
         const title = (
             <span>
@@ -127,29 +160,27 @@ class AddUpdate extends Component {
                         })(<TextArea placeholder="Autosize height based on content lines" autosize />)}
                     </Form.Item>
                     <Form.Item {...formItemLayout} label="商品价格">
-                        {getFieldDecorator('desc', {
+                        {getFieldDecorator('price', {
                             rules: [
                             {
                                 required: true,
                                 message: '请输入商品价格',
-
                             },
+                            {validator: this.validatorPrice}
                             ],
                             initialValue: 1,
-                        })(<Input min={1} max={10} onChange={this.changeValue} addonAfter="元"/>)}
+                        })(<Input type='number' min={1} max={10} onChange={this.changeValue} addonAfter="元"/>)}
                     </Form.Item>
 
                     <Form.Item label="商品分类">
                         {getFieldDecorator('category', {
                             rules: [{ required: true, message: '请选择商品分类!' }],
                         })(
-                            <Select
-                            placeholder="请选择商品分类"
-                            onChange={this.handleSelectChange}
-                            >
-                            <Option value="male">male</Option>
-                            <Option value="female">female</Option>
-                            </Select>,
+                          <Cascader
+                            options={this.state.options} 
+                            loadData={this.loadData}
+                          >
+                          </Cascader>
                         )}
                     </Form.Item>
                     <Form.Item label="商品图片" extra="">
@@ -173,10 +204,10 @@ class AddUpdate extends Component {
                     </div>
                     )}
                     </Form.Item>
-                    <Form.Item wrapperCol={{ span: 12, offset: 6 }}>
-                    <Button type="primary" htmlType="submit" >
-                        提交
-                    </Button>
+                    <Form.Item wrapperCol={{ span: 6, offset: 2 }}>
+                      <Button type="primary" htmlType="submit" >
+                          提交
+                      </Button>
                     </Form.Item>
                 </Form>
             </Card>
